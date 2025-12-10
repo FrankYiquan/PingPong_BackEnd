84a75152fb991c4977e3df09dc0382a02e34b5c5
FrankYiquan <147743539+FrankYiquan@users.noreply.github.com>
2025-12-10 05:17:48 -0500

    redis connection added

--- redis/redisClient.ts
+++ redis/redisClient.ts
@@ -1,6 +1,9 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-});
+export const redis =
+  process.env.REDIS_URL
+    ? new Redis(process.env.REDIS_URL) // Production / Hosted Redis
+    : new Redis({
+        host: process.env.REDIS_HOST,  // Local Redis
+        port: Number(process.env.REDIS_PORT),
+      });
